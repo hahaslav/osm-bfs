c307786c9240f5e2d85f7350f0062db83c0e95f8
Yarik32 <yara-2004@ukr.net>
2020-08-19 12:22:19 +0300

    Замінив версію Leaflet 0.7.7 на 1.6.0

--- main.cpp
+++ main.cpp
@@ -182,9 +182,8 @@ int main()
     fout << "<html>\n";
     fout << "\t<head>\n";
     fout << "\t\t<meta charset=\"utf-8\" />\n";
-    fout << "\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">\n";
-    fout << "\t\t<script src=\"http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js\"></script>\n";
-    fout << "\t\t<link rel=\"stylesheet\" href=\"http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css\" />\n";
+    fout << "\t\t<link rel=\"stylesheet\" href=\"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css\" integrity=\"sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==\" crossorigin=\"\"/>\n";
+    fout << "\t\t<script src=\"https://unpkg.com/leaflet@1.6.0/dist/leaflet.js\" integrity=\"sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==\" crossorigin=\"\"></script>\n";
     fout << "\t\t<style>\n";
     fout << "\t\t\thtml, body {\n";
     fout << "\t\t\t\theight: 100%;\n";
